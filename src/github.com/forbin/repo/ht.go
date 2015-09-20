d1f6b082ef2f9d3fdfcaaf567316974df930ab7d
Lionel G <lionelg3@gmail.com>
2015-09-20 22:56:04 +0200

    Simple http repository

--- src/github.com/forbin/repo/ht.go
+++ src/github.com/forbin/repo/ht.go
@@ -1,1 +1,20 @@
-package repo
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	port := flag.String("port", "8080", "http port")
+	path := flag.String("docroot", ".", "document root")
+	flag.Parse()
+
+	fmt.Println("Start http service")
+	fmt.Println("  port :", *port)
+	fmt.Println("  path :", *path)
+	http.Handle("/", http.FileServer(http.Dir(*path)))
+	http.ListenAndServe(":"+*port, nil)
+}
